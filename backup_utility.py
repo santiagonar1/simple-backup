d7f3181ba247eb2ea18cac5e219bf32ceedab726
Santiago Narvaez Rivas <santiagonar1@gmail.com>
2016-04-21 10:46:01 -0500

    Se crea la clase Files

--- backup_utility.py
+++ backup_utility.py
@@ -6,10 +6,33 @@
 Author: Santiago Narváez Rivas.
 Date: 21-Apr-2016
 """
+import os
+import pwd
+
+
+class File:
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def exists(self):
+        return os.path.exists(self.filepath)
+
+    def size(self):
+        try:
+            return os.path.getsize(self.filepath)
+        except FileNotFoundError:
+            return 0
+
+    def realpath(self):
+        homepath = '/home/' + pwd.getpwuid(os.getuid())[0] + '/'
+        return os.path.dirname(self.filepath.replace(homepath, '', 1))
 
 
 def main():
-    # TODO: Aca va el código
+    file = File('/home/santiago/Documents/Presupuesto.ods')
+    print(file.exists())
+    print(file.size())
+    print(file.realpath())
     return
 
 
