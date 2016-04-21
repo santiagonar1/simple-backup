f077c4bfb831d5cc210ca880ceadcb76fa12188e
Santiago Narvaez Rivas <santiagonar1@gmail.com>
2016-04-21 09:38:57 -0500

    Primer commit

---
+++ backup_utility.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+
+Author: Santiago Narváez Rivas.
+Date: 21-Apr-2016
+"""
+
+
+def main():
+    # TODO: Aca va el código
+    return
+
+
+if __name__ == '__main__':
+    main()
