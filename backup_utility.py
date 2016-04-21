26d162d08e536b3d150dc3e1276cdfc0727d5273
Santiago Narvaez Rivas <santiagonar1@gmail.com>
2016-04-21 18:48:49 -0500

    Clase Backup

--- backup_utility.py
+++ backup_utility.py
@@ -12,24 +12,20 @@ import os
 class Entry:
     def __init__(self, path):
         self.path = path
-        self.size = self.calc_size()
+
+        self.size = None
+        if os.path.isfile(self.path):
+            self.size = os.path.getsize(self.path)
+        elif os.path.isdir(self.path):
+            self.size = sum([file.stat().st_size for file in self.scantree()])
 
     def exists(self):
         return os.path.exists(self.path)
-
-    def calc_size(self):
-        size = None
-        if os.path.isfile(self.path):
-            size = os.path.getsize(self.path)
-        elif os.path.isdir(self.path):
-            size = sum([file.stat().st_size for file in self.scantree()])
-        return size
 
     def scantree(self):
         """
         Permite iterar sobre todos los archivos dentro de la entrada. Esta DEBE ser un directorio
         """
-        assert os.path.isdir(self.path)
         for entry in os.scandir(self.path):
             if entry.is_dir(follow_symlinks=False):
                 yield from Entry(entry.path).scantree()
@@ -49,13 +45,35 @@ class Entry:
             nbytes /= 1000
             i += 1
         f = ('%.1f' % nbytes)
-        return '%s %s' % (f, suffixes[i])
+        return  '{0:.1f} {1}'.format(nbytes, suffixes[i])
+
+class Backup:
+    def __init__(self, entries, destiny):
+        self.entries = entries[:]
+        self.destiny = destiny
+
+    def start(self, use_common_path=True):
+        commonpath = ''
+        if use_common_path:
+            commonpath = os.path.commonpath([f.path for f in self.entries])
+            if commonpath == '/':
+                commonpath = ''
+        for entry in self.entries:
+            dpath = self.destiny + entry.realpath(remove=commonpath)
+            if not os.path.exists(dpath):
+                os.makedirs(dpath)
+            c = 'rsync -avz ' + entry.path + ' ' + dpath
+            os.system(c)
 
 def main():
-    entry = Entry('/home/santiago/Documents')
-    print(entry.exists())
-    print(entry.format_size())
-    print(entry.realpath())
+    filepaths = ['/home/santiago/Documents/Presupuesto.ods',
+             '/home/santiago/Documents/Shortcuts-linux.odt',
+             '/home/santiago/git/Scripts']
+    entries = []
+    for path in filepaths:
+        entries.append(Entry(path))
+    b = Backup(entries, '/tmp/Backup')
+    b.start()
     return
 
 
