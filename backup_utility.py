08e930992dc8970e132371bdcfb2a8feede189c6
Santiago Narvaez Rivas <santiagonar1@gmail.com>
2016-04-21 14:47:59 -0500

    Funcion para calcular tamaño del archivo

--- backup_utility.py
+++ backup_utility.py
@@ -10,27 +10,41 @@ import os
 import pwd
 
 
-class File:
-    def __init__(self, filepath):
-        self.filepath = filepath
+class Entry:
+    def __init__(self, path):
+        self.path = path
+        self.size = self.calc_size()
 
     def exists(self):
-        return os.path.exists(self.filepath)
+        return os.path.exists(self.path)
 
-    def size(self):
-        try:
-            return os.path.getsize(self.filepath)
-        except FileNotFoundError:
-            return 0
+    def calc_size(self):
+        size = None
+        if os.path.isfile(self.path):
+            size = os.path.getsize(self.path)
+        elif os.path.isdir(self.path):
+            size = sum([file.stat().st_size for file in self.scantree()])
+        return size
+
+    def scantree(self):
+        """
+        Permite iterar sobre todos los archivos dentro de la entrada. Esta DEBE ser un directorio
+        """
+        assert os.path.isdir(self.path)
+        for entry in os.scandir(self.path):
+            if entry.is_dir(follow_symlinks=False):
+                yield from Entry(entry.path).scantree()
+            else:
+                yield entry
 
     def realpath(self, remove=''):
-        return os.path.dirname(self.filepath).replace(remove, '', 1)
+        return os.path.dirname(self.path).replace(remove, '', 1)
 
 def main():
-    file = File('/home/santiago')
-    print(file.exists())
-    print(file.size())
-    print(file.realpath())
+    entry = Entry('/home/santiago/Documents')
+    print(entry.exists())
+    print(entry.size)
+    print(entry.realpath())
     return
 
 
