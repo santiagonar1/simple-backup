caade4666e15abd48520aa470aaa33a5e3c7079b
Santiago Narvaez Rivas <santiagonar1@gmail.com>
2016-04-22 09:47:57 -0500

    Se crea la ventana inicial

--- simple_backup.py
+++ simple_backup.py
@@ -7,10 +7,38 @@ Problem description:
 Author: Santiago Narváez Rivas.
 Date:
 """
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gio
+
+class SimpleBackupWindow(Gtk.Window):
+    def __init__(self):
+        # Main Window
+        Gtk.Window.__init__(self, title='Simple Backup')
+        self.set_border_width(10)
+        self.set_default_size(800, 400)
+
+        #Header Bar
+        hb = Gtk.HeaderBar()
+        hb.set_show_close_button(True)
+        hb.props.title = 'Simple Backup'
+        self.set_titlebar(hb)
+
+        button = Gtk.Button(label='Backup')
+        button.connect("clicked", self.on_backup_clicked)
+        hb.pack_end(button)
+
+    def on_backup_clicked(self, button):
+        #TODO: Implementar aquí lógica backup
+        print("Backup clicked")
+
 
 
 def main():
-    # TODO: Aca va el código
+    win = SimpleBackupWindow()
+    win.connect("delete-event", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
     return
 
 
