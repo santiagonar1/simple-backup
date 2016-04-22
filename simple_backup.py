61004feab8f16a7aaec077bc8b68200daa40e782
Santiago Narvaez Rivas <santiagonar1@gmail.com>
2016-04-22 09:15:32 -0500

    Primer commit

---
+++ simple_backup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Problem description:
+
+Author: Santiago Narváez Rivas.
+Date:
+"""
+
+
+def main():
+    # TODO: Aca va el código
+    return
+
+
+if __name__ == '__main__':
+    main()
